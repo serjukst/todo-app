b9282771f13f2c725fbed1812b44960951bfc0ff
Serhii Stepanchuk <serjukst@gmail.com>
2020-09-21 17:58:30 +0300

    feat: change item-add-form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+    onLabelChange = (e) => {
+        console.log(e.target.value)
+    }
 
     render() {
         return (
-            <div className='item-add-form'>
+            <form className='item-add-form d-flex'>
+                <input type="text" 
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="What need to be done?">
+                        </input>
                 <button className='btn btn-outline-secondary'
                         onClick={() => this.props.onItemAdded('Hello World!')}>
                 Add item</button>
-            </div>
+            </form>
         )
     }
 }
